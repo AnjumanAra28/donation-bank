c44732a0dc69771a8582e52c60e6e89d6afc7651
anjumanara28 <anjuanny2024@gmail.com>
2024-09-23 23:09:37 +0100

    home file functionality added

--- js/home.js
+++ js/home.js
@@ -4,58 +4,121 @@ let totalDonateAmount = document.getElementById('total-donate-amount');
 document.getElementById('donate-btn1').addEventListener('click',function(){
     // get donate amount
     const getInputValue = getInputValueById('donate-input1')
+
     if (getInputValue<=0){
        return alert('Negative Amount is not Acceptable');
     }
     else if(isNaN(getInputValue)){
         return alert('Put An Valid Input Number')
     }
+
     let donateValue1 = getTextValueById('donate-amount1')
-    // update card donate value
+    // update card1 donate value
     const newDonateAmount = donateValue1 + getInputValue;
     document.getElementById('donate-amount1').innerText = newDonateAmount;
     // update total donate value
     const totalDonateAmountValue = getTextValueById('total-donate-amount');
     const newTotalDonateAmount = totalDonateAmountValue - getInputValue;
     document.getElementById('total-donate-amount').innerText = newTotalDonateAmount;
+
+    // modal open
+    // document.getElementById('my_modal_5').showModal();
+
+    // create history element
+    const donationArea = 'Donated for Flood at Noakhali, Bangladesh'
+    createHistoryElement(getInputValue ,donationArea);
+
+    // open modal
+    my_modal_5.showModal()
 })
 
 // donate 2 button
 document.getElementById('donate-btn2').addEventListener('click',function(){
     // get donate amount
     const getInputValue = getInputValueById('donate-input2')
+    
     if (getInputValue<=0){
        return alert('Negative Amount is not Acceptable');
     }
     else if(isNaN(getInputValue)){
         return alert('Put A Valid Input Number')
     }
+
     let donateValue1 = getTextValueById('donate-amount2')
-    // update card donate value
+    // update card2 donate value
     const newDonateAmount = donateValue1 + getInputValue;
     document.getElementById('donate-amount2').innerText = newDonateAmount;
     // update total donate value
     const totalDonateAmountValue = getTextValueById('total-donate-amount');
     const newTotalDonateAmount = totalDonateAmountValue - getInputValue;
     document.getElementById('total-donate-amount').innerText = newTotalDonateAmount;
+
+    // create history section
+    const donationArea = 'Donated for Flood Relief in Feni,Bangladesh'
+    createHistoryElement(getInputValue ,donationArea);
+
+    // open modal
+    my_modal_5.showModal()
 })
 
 // donate 3 button
 document.getElementById('donate-btn3').addEventListener('click',function(){
     // get donate amount
     const getInputValue = getInputValueById('donate-input3')
+
     if (getInputValue<=0){
        return alert('Negative Amount is not Acceptable');
     }
     else if(isNaN(getInputValue)){
         return alert('Put A Valid Input Number')
     }
+
     let donateValue1 = getTextValueById('donate-amount3')
-    // update card donate value
+    // update card3 donate value
     const newDonateAmount = donateValue1 + getInputValue;
     document.getElementById('donate-amount3').innerText = newDonateAmount;
     // update total donate value
     const totalDonateAmountValue = getTextValueById('total-donate-amount');
     const newTotalDonateAmount = totalDonateAmountValue - getInputValue;
     document.getElementById('total-donate-amount').innerText = newTotalDonateAmount;
+
+    // create history section
+    const donationArea = 'Donated for Injured in the Quota Movement'
+    createHistoryElement(getInputValue ,donationArea);
+
+    // open modal
+    my_modal_5.showModal() 
+})
+
+// show history section and hide donation section
+document.getElementById('history-btn').addEventListener('click',function(){
+    const historySection = document.getElementById('history-section');
+    historySection.classList.remove('hidden');
+    const donationSection = document.getElementById('donation-card-section');
+    donationSection.classList.add('hidden')
+    // history btn background change
+    const historyBtn = document.getElementById('history-btn');
+    historyBtn.classList.remove('btn-outline')
+    historyBtn.classList.add('bg-primary')
+    // donation btn background change
+    const donationBtn = document.getElementById('donation-btn');
+    donationBtn.classList.add('btn-outline');
+    donationBtn.classList.remove('bg-primary');
+
+})
+
+// show donation section and hide history section
+document.getElementById('donation-btn').addEventListener('click',function(){
+    const donationSection = document.getElementById('donation-card-section');
+    donationSection.classList.remove('hidden')
+    const historySection = document.getElementById('history-section');
+    historySection.classList.add('hidden');
+    // donation btn background change
+    const donationBtn = document.getElementById('donation-btn');
+    donationBtn.classList.remove('btn-outline');
+    donationBtn.classList.add('bg-primary');
+    // history btn background change
+    const historyBtn = document.getElementById('history-btn');
+    historyBtn.classList.remove('bg-primary');
+    historyBtn.classList.add('btn-outline');
 })
--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,4 @@
-
+// input value get
 function getInputValueById(id){
     const inputField = document.getElementById(id);
     const inputFieldValue = parseFloat(inputField.value);
@@ -7,8 +7,21 @@ function getInputValueById(id){
     
 }
 
+// text field value
 function getTextValueById (id){
     const textField = document.getElementById(id);
     const textFieldValue = parseFloat(textField.innerText);
     return textFieldValue;
 }
+
+// create history section 
+function createHistoryElement (id , string){
+    const historySection = document.getElementById('history-section');
+    const donationHistory = document.createElement('div');
+    donationHistory.innerHTML = ` ${id} BDT is  ${string} .` 
+    donationHistory.setAttribute('class', 'font-bold border text-center rounded-xl p-6 shadow-xl w-9/12 mx-auto mb-5' )
+    historySection.append(donationHistory);
+    return historySection;
+}
+
+
